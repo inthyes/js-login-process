7bec740de924650f6bd3fe433cfded18a77f66e3
inthyes <127703931+inthyes@users.noreply.github.com>
2023-03-21 20:08:37 +0900

    돔 이용하여 html객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,21 @@
 "use strict";
 
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
 
-console.log("hello");
-console.log("byes");
+loginBtn.addEventListener("click",login);
+
+function login(){
+    // const req={
+    //     id : id.value,
+    //     psword : psword.value,
+    // };
+    // console.log(req);
+
+    const req ={
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);  
+}
