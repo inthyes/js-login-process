8683a0323a29b09c0339cf7abf139a185311b150
inthyes <127703931+inthyes@users.noreply.github.com>
2023-03-21 23:29:13 +0900

    User 모델 구현

--- app/src/routes/home/home.controller.js
+++ app/src/routes/home/home.controller.js
@@ -1,7 +1,6 @@
-"user strict";
+"use strict";
 
-const UserStorage = require("../../models/UserStorage");
-
+const User = require("../../models/User");
 
 const output ={
     home:(req,res)=>{
@@ -10,7 +9,7 @@ const output ={
     login:(req,res)=>{
         res.render("home/login");
     },
-}
+};
 
 // const home = (req, res)=>{
 //     res.render("home/index");
@@ -21,26 +20,29 @@ const output ={
 // };
 
 const process={
-    login:(req,res)=>{
-        const id=req.body.id,
-            psword=req.body.psword;
-        // console.log(id,psword);       
+    login:(req,res)=> {
+        const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+        return res.json(response);
 
-        // const userStorage = new UserStorage();
-    const users = UserStorage.getUsers("id","psword");
+    //     const id=req.body.id,
+    //         psword=req.body.psword;
+        
+    // const users = UserStorage.getUsers("id","psword");
 
-    const response ={};
-    if(users.id.includes(id)){
-        const idx = users.id.indexOf(id);
-        if(users.psword[idx]===psword){
-            response.success = true;
-            return res.json(response);
-        }
-    }
+    // const response ={};
+    // if(users.id.includes(id)){
+    //     const idx = users.id.indexOf(id);
+    //     if(users.psword[idx]===psword){
+    //         response.success = true;
+    //         return res.json(response);
+    //     }
+    // }
 
-    response.success=false;
-    response.msg="로그인에 실패했지롱";
-    return res.json(response);
+    // response.success=false;
+    // response.msg="로그인에 실패했지롱";
+    // return res.json(response);
     },
 };
 
