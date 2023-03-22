552f47ea196b19717b5b3be5ef1864961817d11d
inthyes <127703931+inthyes@users.noreply.github.com>
2023-03-22 16:40:16 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,16 +8,22 @@ class User{
     }
 
     login(){
-        const body = this.body;
-        const{id, psword} = UserStorage.getUserInfo(body.id);
+        const client = this.body;
+        const{id, psword} = UserStorage.getUserInfo(client.id);
       
         if (id){
-            if (id === body.id && psword === body.psword){
+            if (id === client.id && psword === client.psword){
                 return{success:true};
             }
             return {success: false, msg:"비밀번호가 틀렸음"};
         }
         return {success: false, msg:"존재하지 않는 아이디임"};
+    }
+
+    register(){
+        const client = this.body;
+        const response = UserStorage.save(this.body);
+        return response;
     }
 }
 module.exports=User;
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,31 +9,34 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click",register);
 
 function register(){
+    if(!id.value) return alert("아이디 입력해라.");
+    if(psword.value !== confirmPsword.value){
+        return alert("비밀번호가 일치하지 않습니다.");
+    }
 
     const req ={
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };
-    console.log(req);
 
-    // fetch("/login",{
-    //     method: "post",
-    //     headers: {
-    //         "content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(req),
-    // })
-    //     .then((res) => res.json())
-    //     .then((res)=>{
-    //         if(res.success){
-    //             location.href="/";
-    //         } else{
-    //             alert(res.msg);
-    //         }
-    //     })
-    //     .catch((err)=>{
-    //         console.error(new Error("로그인 중 에러"));
-    //     });
+
+    fetch("/register",{
+        method: "post",
+        headers: {
+            "content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then((res)=>{
+            if(res.success){
+                location.href="/login";
+            } else{
+                alert(res.msg);
+            }
+        })
+        .catch((err)=>{
+            console.error(new Error("회원가입 중 에러"));
+        });
 } 
