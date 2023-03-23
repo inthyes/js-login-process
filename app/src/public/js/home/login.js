7187e7db212aa5783190b631df6e45a9d0cbf57f
곽채연 <cheyeon001214@naver.com>
2023-03-24 00:51:13 +0900

    회원가입 요청 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,7 +2,7 @@
 
 const id = document.querySelector("#id"), //id 태그로 부여되어있는 id를 불러와라
 psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("button");
+loginBtn = document.querySelector("#button");
 
 loginBtn.addEventListener("click", login);
 
