13a1ba24ce223d64c78b5f6616616f2918210f06
곽채연 <cheyeon001214@naver.com>
2023-03-23 17:40:25 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -18,5 +18,15 @@ function login() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(req), 
-    }).then((res)=> res.json()).then((res)=> console.log(res));
+    }).then((res)=> res.json())
+    //.then((res)=> console.log(res)); 을 생략가능
+    .then((res) => {
+        if(res.success) {
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    }).catch((err)=> {
+        console.error("로그인 중 에러 발생");
+    });
 }
