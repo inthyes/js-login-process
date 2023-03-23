e96799c875722366b184658cbf7c3bcd544ae84b
곽채연 <cheyeon001214@naver.com>
2023-03-23 19:13:07 +0900

    user모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output ={
     hello : (req, res) =>{
@@ -14,21 +15,8 @@ const output ={
 
 const process = {
     login : (req, res) => {
-        const id = req.body.id,
-        psword = req.body.psword;
-    //const userStorage = new UserStorage();
-    const users = UserStorage.getUsers("id","psword");
-
-        const response = {};
-        if(users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다";
+        const user = new User(req.body); //여기서의 body가 User클래스의 body로 들어감
+        const response = user.login();
         return res.json(response);
 
     },
@@ -38,3 +26,4 @@ module.exports = {
     output,
     process,
 };
+
