eed67e3fad85fc5b68b257480e25ddf83baf50d2
inthyes <127703931+inthyes@users.noreply.github.com>
2023-03-29 17:58:36 +0900

    최종

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,16 +6,16 @@ const User = require("../../models/User");
 
 const output ={
     home : (req, res) =>{
-        logger.info(`GET / 200 "홈 화면으로 이동"`);
+        logger.info(`GET / 304 "홈 화면으로 이동"`);
         res.render("home/index");
     },
     
     login : (req,res) => {
-        logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+        logger.info(`GET /login 304 "로그인 화면으로 이동"`);
         res.render("home/login");
     },
     register : (req, res) => {
-        logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+        logger.info(`GET /register 304 "회원가입 화면으로 이동"`);
         res.render("home/register");
     }
 };
@@ -24,31 +24,27 @@ const process = {
     login : async (req, res) => {
         const user = new User(req.body); //여기서의 body가 User클래스의 body로 들어감
         const response = await user.login();
-        if(response.err)
-            logger.error(
-                `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
-                );
-        else
-            logger.info(
-                `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-                );
-        return res.json(response);
+
+        const url = {
+            method: "POST",
+            path: "/login",
+            status: response.err ? 400 : 200,
+        };
+        log(response, url);
+        return res.status(url.status).json(response);
     },
     register: async (req, res) => {
         const user = new User(req.body); //여기서의 body가 User클래스의 body로 들어감
         const response = await user.register();
-        if (response.err)
-            logger.error(
-                `POST /register 200 Response: "success: ${response.success}, ${response.err}"`
-                );
-        else
-            logger.info(
-                `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-                );
-              
 
+        const url = {
+            method: "POST",
+            path: "/register",
+            status: response.err ? 400 : 201,
+        };
 
-        return res.json(response);
+        log(response, url);
+        return res.status(url.status).json(response);
 
     },
 };
@@ -58,3 +54,13 @@ module.exports = {
     process,
 };
 
+const log = (response, url) =>{
+    if(response.err){
+            logger.error(
+                `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}"`
+                );}
+        else{
+            logger.info(
+                `${url.method} ${url.path} ${url.status} Response: ${response.success}, msg: ${response.msg || " "}"`
+                );}
+}
